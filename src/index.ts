8b7c6caf0532c6ed3b28af5df4d74f783a6e68fc
och-och <och.hco.och.hco@gmail.com>
2024-06-03 09:57:49 +0300

    add touch support

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ export class DragHandle {
 	private abortController = new AbortController()
 	private offsetX = 0
 	private offsetY = 0
-	private dragSubscribers = new Set<(event:MouseEvent)=>void>()
-	private dropSubscribers = new Set<(event:MouseEvent)=>void>()
-	private moveSubscribers = new Set<(event:MouseEvent)=>void>()
+	private dragSubscribers = new Set<(event:MouseEvent|TouchEvent)=>void>()
+	private dropSubscribers = new Set<(event:MouseEvent|TouchEvent)=>void>()
+	private moveSubscribers = new Set<(event:MouseEvent|TouchEvent)=>void>()
 
 	/**
 	 * Create a draggable element
@@ -15,32 +15,57 @@ export class DragHandle {
 	 */
 	constructor(public element: HTMLElement) {
 		this.element.classList.add("pup-draggable")
+		this.element.addEventListener("touchstart", e => this.triggerDrag(e))
 		this.element.addEventListener("mousedown", e => this.triggerDrag(e))
 	}
 
-	triggerDrag(event: MouseEvent) {
-		this.offsetX = event.offsetX
-		this.offsetY = event.offsetY
-		this.element.style.position = "absolute"
-		this.updatePosition(event.pageX, event.pageY)
+	triggerDrag(event: MouseEvent|TouchEvent) {
+		const touch = this.getTouch(event)
+		if (!touch) return
 
-		this.abortController = new AbortController()
-		document.addEventListener("mousemove", e => this.triggerMove(e), {
-			signal: this.abortController.signal,
-		})
-		document.addEventListener("mouseup", e => this.triggerDrop(e), {
-			signal: this.abortController.signal,
-		})
+		if (event instanceof MouseEvent) {
+			if (event.buttons != 1) return
+
+			this.offsetX = event.offsetX
+			this.offsetY = event.offsetY
+			this.abortController = new AbortController()
+			document.addEventListener("mousemove", e => this.triggerMove(e), {
+				signal: this.abortController.signal,
+			})
+			document.addEventListener("mouseup", e => this.triggerDrop(e), {
+				signal: this.abortController.signal,
+			})
+		}
+		else {
+			const box = this.element.getBoundingClientRect()
+			this.offsetX = box.width / 2
+			this.offsetY = box.height / 2
+			document.addEventListener("touchmove", e => this.triggerMove(e), {
+				signal: this.abortController.signal,
+			})
+			document.addEventListener("touchend", e => this.triggerDrop(e), {
+				signal: this.abortController.signal,
+			})
+			document.addEventListener("touchcancel", e => this.triggerDrop(e), {
+				signal: this.abortController.signal,
+			})
+		}
+
+		this.element.style.position = "absolute"
+		this.updatePosition(touch.pageX, touch.pageY)
+
 		this.dragSubscribers.forEach(fn => fn(event))
 	}
 
-	triggerDrop(event: MouseEvent) {
+	triggerDrop(event: MouseEvent|TouchEvent) {
 		this.abortController.abort()
 		this.dropSubscribers.forEach(fn => fn(event))
 	}
 
-	triggerMove(event: MouseEvent) {
-		this.updatePosition(event.pageX, event.pageY)
+	triggerMove(event: MouseEvent|TouchEvent) {
+		const touch = this.getTouch(event)
+		if (!touch) return
+		this.updatePosition(touch.pageX, touch.pageY)
 		this.moveSubscribers.forEach(fn => fn(event))
 	}
 
@@ -49,11 +74,16 @@ export class DragHandle {
 		this.element.style.top = y - this.offsetY + "px"
 	}
 
+	getTouch(event: MouseEvent|TouchEvent) {
+		const touch = event instanceof MouseEvent ? event : event.touches.item(0)
+		return touch
+	}
+
 	/**
 	 * Add an event handler for drag starting
 	 * @returns a function for unsubscribing
 	 */
-	onDrag(handler: (event:MouseEvent) => void) {
+	onDrag(handler: (event:MouseEvent|TouchEvent) => void) {
 		this.dragSubscribers.add(handler)
 		return () => this.dragSubscribers.delete(handler)
 	}
@@ -62,7 +92,7 @@ export class DragHandle {
 	 * Add an event handler for drag ending
 	 * @returns a function for unsubscribing
 	 */
-	onDrop(handler: (event:MouseEvent) => void) {
+	onDrop(handler: (event:MouseEvent|TouchEvent) => void) {
 		this.dropSubscribers.add(handler)
 		return () => this.dropSubscribers.delete(handler)
 	}
@@ -71,7 +101,7 @@ export class DragHandle {
 	 * Add an event handler for moving the mouse while dragging
 	 * @returns a function for unsubscribing
 	 */
-	onMove(handler: (event:MouseEvent) => void) {
+	onMove(handler: (event:MouseEvent|TouchEvent) => void) {
 		this.moveSubscribers.add(handler)
 		return () => this.moveSubscribers.delete(handler)
 	}
